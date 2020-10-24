049b1cb9bdfd77c2ffe2eafeb7b3bfcb560e8590
Simo Mafuxwana <simodms@gmail.com>
2020-10-24 20:42:34 +0200

    new conflist

--- src/components/ConfList.vue
+++ src/components/ConfList.vue
@@ -1,57 +1,40 @@
 <template>
-  <ion-list>
-    <div v-for="(item, index) in data" :key="index">
-      <ion-item>
-        <!-- <template v-slot:start> -->
-        <ion-text
-          slot="start"
-          :class="item.status === 'canceled' ? `left event-canceled` : 'left desktop-only'"
-        >
-          <ion-icon class="left-icons" :icon="micOutline"></ion-icon>
-        </ion-text>
-        <!-- </template> -->
-        <ion-label>
-          <h2 :style="`color: ${item.color}`">{{ item.eventName }}</h2>
-          <p>{{ item.venue }}, {{ item.town }}</p>
-        </ion-label>
-        <ion-text class="visit-website">
+  <ion-grid>
+    <ion-row v-for="(item, index) in data" :key="index">
+      <ion-col>
+        <b>{{ item.eventName }}</b>
+        <p>{{ item.venue }}, {{ item.town }}</p>
+
+        <p v-if="item.start && item.end">
+          {{ `${item.start} - ${item.end}` }}
+        </p>
+
+        <p v-if="!item.start && !item.end" class="no-date">New dates TBA</p>
+
+        <p class="mobile-only">
           <a :href="item.website" target="_blank">
             Website <ion-icon :icon="openOutline"></ion-icon>
-          </a>
-        </ion-text>
-      </ion-item>
-      <ion-item class="dates">
-        <ion-label>
-          <b v-if="item.start && item.end">
-            {{ `${item.start} - ${item.end}` }}
-          </b>
-          <p v-if="!item.start && !item.end" class="no-date">New dates TBA</p>
-          <!-- <p v-if="item.dates">
-            <span v-for="(date, idx) in item.dates" :key="idx">
-              <b>{{ date.date }}</b> {{`${date.startTime}-${date.endTime}`}}
-              <ion-icon :icon="chevronForward"></ion-icon>
-            </span>
-          </p> -->
-        </ion-label>
-        <ion-text class="actions" v-if="profilePage">
-          <ion-button color="dark" @click="editEvent(item)"> Edit </ion-button>
-          <ion-button color="dark" @click="deleteEvent(item.id)">
-            Delete
-          </ion-button>
-        </ion-text>
-      </ion-item>
-    </div>
-  </ion-list>
+          </a
+        ></p>
+      </ion-col>
+      <ion-col offset="2" class="desktop-only">
+        
+      </ion-col>
+      <ion-col class="desktop-only">
+        <a :href="item.website" target="_blank">
+          Website <ion-icon :icon="openOutline"></ion-icon>
+        </a>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
 </template>
 
 <script>
 import {
-  IonList,
-  IonItem,
-  IonLabel,
-  IonText,
   IonIcon,
-  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
   modalController
 } from "@ionic/vue";
 import { chevronForward, micOutline, openOutline } from "ionicons/icons";
@@ -68,12 +51,10 @@ export default {
     },
   },
   components: {
-    IonList,
-    IonItem,
-    IonLabel,
-    IonText,
+    IonGrid,
+    IonRow,
+    IonCol,
     IonIcon,
-    IonButton
   },
   setup() {
     return {
@@ -136,62 +117,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h2 {
-  font-weight: 600;
-  color: #000000;
-}
-
-ion-item {
-  --background: #ffffff;
-  margin: 5px 0 0 -15px;
-  line-height: 50px;
-  border-radius: 5px;
-}
-
-ion-item.dates {
-  --background: #ffffff;
-  margin: -15px 0 0 auto;
-  border-radius: 0 0 5px 5px;
-  line-height: 20px;
-  padding: 0 0 0 44px;
+ion-row {
+  background: #ffffff;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-left: 10px solid #226391;
 }
 
 .no-date {
   font-style: italic;
 }
 
-ion-text.left {
-  padding: 15px 10px 0px 10px;
-  color: #000;
-  border-radius: 5px 0 0 5px;
-  margin: 0 15px 0 -2px;
-  // background: #36AEEA;
-
-  &:nth-of-type(odd) {
-    background: #226391;
-  }
-
-  :nth-of-type(even) {
-    background: #36aeea;
-  }
-
-  ion-icon.left-icons {
-    color: #ffffff;
-    font-size: 25px;
-  }
-}
-
-ion-text.visit-website {
-  // font-size: 13px;
-  color: #626262;
-
-  a {
-    text-decoration: none;
-  }
-}
-
-ion-icon.right-icons {
-  color: #36aeea;
-  margin-left: 5px;
+a {
+  text-decoration: none;
 }
 </style>
